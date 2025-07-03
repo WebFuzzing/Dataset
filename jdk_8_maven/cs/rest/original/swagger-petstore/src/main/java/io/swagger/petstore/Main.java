087be3d12aba5d4ce87adbafccc41ed9ea33d716
Omur <omur@visseria.com>
2025-07-03 22:07:09 +0300

    trying on another computer

--- jdk_8_maven/cs/rest/original/swagger-petstore/src/main/java/io/swagger/petstore/Main.java
+++ jdk_8_maven/cs/rest/original/swagger-petstore/src/main/java/io/swagger/petstore/Main.java
@@ -1,32 +1,11 @@
-//MODIFIED: This code is a simple Java application that starts an embedded Tomcat server to serve the Swagger Petstore application.
-
 package io.swagger.petstore;
 
-import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.File;
-
+@SpringBootApplication
 public class Main {
-    public static void main(String[] args) throws Exception {
-        int port = 8080;
-
-        if (args.length > 0) {
-            try {
-                port = Integer.parseInt(args[0]);
-            } catch (NumberFormatException e) {
-                System.err.println("Invalid port number: " + args[0] + ". Using default port 8080.");
-            }
-        }
-
-        Tomcat tomcat = new Tomcat();
-        tomcat.setPort(port);
-        tomcat.getConnector();
-
-        String webappDirLocation = "src/main/webapp";
-        tomcat.addWebapp("", new File(webappDirLocation).getAbsolutePath());
-
-        System.out.println("Swagger Petstore running at http://localhost:" + port);
-        tomcat.start();
-        tomcat.getServer().await();
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
     }
 }
